47a9fdfc64d88f9c664e6850e5f2c15726e8815a
Eric Lindell <eric.lindell@avensia.com>
2023-04-17 20:11:20 +0200

    add remaps for window navigation

--- lua/lindelleric/remap.lua
+++ lua/lindelleric/remap.lua
@@ -1,7 +1,11 @@
 vim.g.mapleader = " "
 
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- -- windows movements
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
 
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
