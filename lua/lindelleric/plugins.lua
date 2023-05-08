3017e231ca9f505b02b4c7c6a6fb4046ae1f97ce
Eric Lindell <eric.lindell@avensia.com>
2023-05-08 11:18:29 +0200

    add auto closing tag with treesitter

--- lua/lindelleric/plugins.lua
+++ lua/lindelleric/plugins.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
 
   use ( "nvim-treesitter/nvim-treesitter" , {run = ':TSUpdate'} )
   use('nvim-treesitter/playground')
+  use('windwp/nvim-ts-autotag')
 
   use('theprimeagen/harpoon')
 
